8afea96fa30bb7ae5e793d7ad733dcb463e2a06c
robertpfaff <42342840+robertpfaff@users.noreply.github.com>
2022-07-18 09:32:54 -0400

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,32 +1,40 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
-const Immutable = require('immutable');
 const fetch = require('node-fetch')
 const path = require('path')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 const port = 3000
 
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use('/', express.static(path.join(__dirname, '../public')))
 
-// share chosen rover in session storage
-chosenRover = 'curiosity'
+// your API calls
 
-// Backend NASA API calls for rover photo data.
 app.get('/gallery/:rover', async (req, res) => {
+
+    const chosenRover = req.params['rover']
+    console.log("Chosen Rover")
+    console.log("Response:", rover) // produces error Response = rover, not rover name, but the key 'rover.'
+    console.log("Request Params Rover")
+    console.log(req.query)
+    console.log("Request ALL Params")
+    console.log(req.params)
+
     try {
-        let gallery = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${chosenRover}/latest_photos?api_key=${process.env.API_KEY}`)
+        let gallery = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
         res.send({ gallery })
         console.log(gallery)
     } catch (err) {
         console.log('error:', err);
     }
-})
+});
 
 // example API call
 app.get('/apod', async (req, res) => {
