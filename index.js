8395926ba9cf70891a34d4e6858279de3f5b840b
robertpfaff <42342840+robertpfaff@users.noreply.github.com>
2022-07-22 09:06:23 -0400

    Successful NASA api 7/22 9am est

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path')
+const path = require('path');
+const fetch = require('node-fetch');
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, '../public')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/formdata', (req, res) => {
-  res.send(req.body.rover);
+app.post('/formdata', async (req, res) => {
+
+  const rover = req.body.rover
+
+  try {
+    let gallery = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=DH46IQlx0gMyXPmLAgkxXDSPo2OrbIjPs8OJLj6L`)
+    .then(res => res.json())
+    res.send({ gallery })
+    console.log(gallery)
+  } catch (err) {
+    console.log('error:', err);
+  }
 });
 
 const port = 3000;
